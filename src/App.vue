a5b725b83bb4547e417ddbed6e23a8487e4dc656
Jake Schweihs <jschweihs@gmail.com>
2019-05-10 23:51:53 -0500

    More blocking out

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <!-- HTML SECTION -->
   <div id="app">
-    <nav-bar/>
-    <p>Coming soon...</p>
+      <nav-bar/>
+
+      <div class="image">
+
+      </div>
+      <div class="hero">
+
+      </div>
+      <div class="bio">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +29,7 @@ export default {
   data () {
     return {
       
-    }
+    };
   }
 }
 </script>
@@ -30,9 +39,65 @@ export default {
 <style>
 /* CSS SECTION */
 
-/* Css coming soon */
+* { padding: 0; margin: 0;}
+
 html {
+  font-family: 'Karla', sans-serif;
+}
+
+h1,h2,h3,h4,h5,h6 {
+  padding: 20px;
+
+}
+h1 {
   font-family: 'Fjalla One', sans-serif;
+}
+
+.container {
+  max-width: 992px;
+  margin: 0 auto;
+}
+
+.image {
+  width: 100%;
+  height: 600px;
+}
+.hero {
+  width: 992px;
+  height: 400px;
+  margin: 0 auto;
+  position: absolute;
+  z-index: 100;
+  left: calc((100% - 992px) / 2);
+  background-color: white;
+  border: 4px solid #d0d0ff;
+  border-radius: 20px;
+}
+
+/* BIO */
+.bio {
+  margin-top: 60px;
+  display: block;
+  width: 100%;
+  height: 500px;
+  background-color: #4040ff;
+}
+
+.bio:after {
+    content: " ";
+    display: block;
+    /* position: absolute; */
+    /* z-index: -1; */
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    background: white;
+    transform-origin: top right;
+    -ms-transform: skew(85deg, 0deg);
+    -webkit-transform: skew(85deg, 0deg);
+    transform: skew(85deg, 0deg);
+    -webkit-backface-visibility: hidden;
 }
 
 </style>
--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,18 +1,127 @@
 <template>
-    <div>
-        <h1 class="nav-title">
-            Full Stack Developer and Artist
-        </h1>
-        <h2 class="nav-subtitle">
-            I strive to create elegant, clean, reusable, and readable code.
-        </h2>
+    <div class="container">
+        <div class="nav-bar">
+            <div class="nav-header">
+                <div class="logo">
+                    <a class="logo-anchor" href="/">
+                        <img class="logo-img" src="/images/logo.png"/>
+                    </a>
+                </div>
+                <div class="nav-links">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="/">
+                                Portfolio
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/">
+                                Artwork
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/">
+                                Blog
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h1 class="nav-title">
+                Full Stack Web Developer and Artist
+            </h1>
+            <h3 class="nav-subtitle">
+                I strive to create elegant, clean, reusable, and readable code.
+            </h3>
+        </div>
     </div>
 </template>
 
 
 
 <style scoped>
-    .nav-title {
-        font-size: 32px;
+
+    .nav-bar {
+        padding: 20px 0;
     }
+    .nav-header {
+        display: block;
+        padding: 20px 0;
+    }
+
+    .logo {
+        width: 80px;
+        height: 80px;
+        display: inline-block;
+    }
+
+    .logo-anchor {
+        padding: 10px;
+        display: inline-block;
+    }
+
+    .nav-links {
+        
+        display: inline-block;
+        margin-left: 60px;
+    }
+
+    .nav-list {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .nav-item {
+        float: left;
+        display: block;
+        text-align: center;
+        font-size: 18px;
+        margin: 0 12px;
+    }
+
+    .nav-item a {
+        text-decoration: none;
+        color: #4040ff;
+        transition: .25s;
+    }
+
+    .nav-item a:hover {
+        color: orange;
+    }
+
+    .navitem li:first-child a { 
+        border-left: 10px solid #3498db; 
+    }
+
+    .nav-item a:after {
+        content: "";
+        display: block;
+        margin: 0 auto;
+        width: 0;
+        border-bottom: 1px solid orange;
+        transition: all 0.3s ease 0s; 
+        -webkit-transition: all 0.3s ease 0s; 
+    }
+
+    .nav-item a:hover:after {
+        width: 100%;
+    }
+
+    .nav-title {
+        font-size: 48px;
+        text-align: center;
+    }
+
+    .nav-subtitle {
+        text-align: center;
+        font-weight: 100;
+        color: #333;
+    }
+
 </style>
