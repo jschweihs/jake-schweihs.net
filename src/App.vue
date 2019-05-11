837c74dc9f38f5801ffcc6cb142eec451191a1a0
Jake Schweihs <jschweihs@gmail.com>
2019-05-10 19:17:12 -0500

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- HTML SECTION -->
+  <div id="app">
+    <nav-bar/>
+    <p>Coming soon...</p>
+  </div>
+</template>
+
+
+
+<script>
+// JAVASCRIPT SECTION
+import NavBar from './components/nav/NavBar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+
+<style>
+/* CSS SECTION */
+
+/* Css coming soon */
+html {
+  font-family: 'Fjalla One', sans-serif;
+}
+
+</style>
---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="nav-title">
+            Full Stack Developer and Artist
+        </h1>
+        <h2 class="nav-subtitle">
+            I strive to create elegant, clean, reusable, and readable code.
+        </h2>
+    </div>
+</template>
+
+
+
+<style scoped>
+    .nav-title {
+        font-size: 32px;
+    }
+</style>
